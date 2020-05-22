7603dd7f7ae607c851d01db3a91a483f16d28e15
Mary Waithera <njihiamary11@gmail.com>
2020-05-22 15:46:29 +0300

    create picture, location, and category models to facilitate the creation of objects and create first migration

--- pictures/models.py
+++ pictures/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+
+    """
+    class facilitates the creation of location objects
+    """
+    location_name = models.CharField(max_length=70)
+
+    def __str__(self):
+        return self.location_name
+
+class Category(models.Model):
+    
+    """
+    class facilitates the creation of category objects
+    """
+    category_name = models.CharField(max_length=70)
+
+    def __str__(self):
+        return self.category_name
+
+class Image(models.Model):
+
+    """
+    class facilitates the creation of image objects
+    """
+    image_name = models.CharField(max_length=70)
+    image_description = models.TextField(max_length=200)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    category = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.image_name
+
+
