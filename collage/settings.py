c0f579b6cf1ab68ea7e6737063852999e5ae2f2d
Mary Waithera <njihiamary11@gmail.com>
2020-05-22 13:08:57 +0300

    create database settings to facilitate the use of postgresql instead of sqlite

--- collage/settings.py
+++ collage/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'collage.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DT_NAME'),
+        'USER': os.environ.get('DT_USER'),
+        'PASSWORD': os.environ.get('DT_PASSWORD'),
     }
 }
 
