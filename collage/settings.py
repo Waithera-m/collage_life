7bf5d8f24a8859ffcd44f5e637fdd095d7051a68
Mary Waithera <njihiamary11@gmail.com>
2020-05-22 14:56:52 +0300

    create base template from which all other templates will inherit their html structure; create template that will be responsible for displaying the landing page and create route to this template

--- collage/settings.py
+++ collage/settings.py
@@ -22,6 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
+print(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pictures.apps.PicturesConfig'
+    'pictures.apps.PicturesConfig',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
--- pictures/views.py
+++ pictures/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+
+    """
+    view function returns the landing page
+    """
+    return render(request,'all_pictures/index.html')
